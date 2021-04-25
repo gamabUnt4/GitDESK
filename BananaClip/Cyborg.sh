e39aa02ed94490be1fa58da3dc899fe5feb0ea8b
gamabunta <newtgigadrain@gmail.com>
2021-04-24 21:08:06 -0400

    Linux "if" statements and "delimeters"

---
+++ BananaClip/Cyborg.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+#this is a here Doc Delimiter script
+#cyborg is the delimiter
+
+count=3
+
+if [ $count -eq 3 ]
+
+then
+
+cat << cyborg
+echo Viktor Vaughns large awning
+echo underneath sits Victor Stone
+cyborg
+
+else
+
+cat << grobyc
+echo Victor Stone’s huge home
+echo Inside sits Viktor Vaughn
+grobyc
+
+fi
---
+++ BananaClip/ghostcode.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+echo Yellow Neon Navy Apple Dope >Oh.txt
+echo Borax Onyx Yellow >> Oh.txt
+
+echo Who Calls?
+
+
+count=19
+
+if [ $count -eq 9 ]
+then
+
+	echo “TEN”
+
+else
+       echo “NOT ON TEN “
+fi
